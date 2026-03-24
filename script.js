5080796d1ff3ae59db05d13fa6b516e73b42da14
fiufnvanfiufn <vanyachich@outlook.com>
2026-03-25 01:15:43 +0300

    fix css and html

--- script.js
+++ script.js
@@ -1,269 +1,183 @@
-window.onload = function(){
+window.onload = function() {
 
-let a = ''
-let b = ''
-let expressionResult = ''
-let selectedOperation = null
-let isAccumulativeMode = false
-let lastResult = ''
+    let a = ''
+    let b = ''
+    let expressionResult = ''
+    let selectedOperation = null
+    let isAccumulativeMode = false
+    let lastResult = ''
 
-outputElement = document.getElementById("result")
+    const outputElement = document.getElementById("oracle_output")
+    const digitButtons = document.querySelectorAll('[id ^= "star_"]')
 
-digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+    function fact(n) {
+        if (n < 0) return 'Ошибка'
+        if (n === 0 || n === 1) return 1
+        let res = 1
+        for (let i = 2; i <= n; i++) res *= i
+        return res
+    }
 
-function fact(n) {
-    if (n == 0) return 1
-    return fact(n - 1)
-}
-
-function onDigitButtonClicked(digit) {
-    if (!selectedOperation) {
-        if ((digit != '.') || (digit == '.' && !a.includes(digit))) {
-            a += digit
+    function onDigitButtonClicked(digit) {
+        if (!selectedOperation) {
+            if ((digit !== '.') || (digit === '.' && !a.includes(digit))) {
+                a += digit
+            }
+            outputElement.innerHTML = a
+        } else {
+            if ((digit !== '.') || (digit === '.' && !b.includes(digit))) {
+                b += digit
+                outputElement.innerHTML = b
+            }
         }
-        outputElement.innerHTML = a
-    } else {
-        if ((digit != '.') || (digit == '.' && !b.includes(digit))) {
-            b += digit
+    }
+
+    digitButtons.forEach(button => {
+        button.onclick = function() {
+            const digitValue = button.innerHTML
+            onDigitButtonClicked(digitValue)
+        }
+    });
+
+    document.getElementById("orbit_mult").onclick = function() {
+        if (a === '') return
+        selectedOperation = 'x'
+    }
+
+    document.getElementById("orbit_plus").onclick = function() {
+        if (a === '') return
+        if (selectedOperation === '+') {
+            isAccumulativeMode = true
+            if (b === '') b = a
+            calculateResult()
+        } else {
+            isAccumulativeMode = false
+        }
+        selectedOperation = '+'
+        b = ''
+    }
+
+    document.getElementById("orbit_minus").onclick = function() {
+        if (a === '') return
+        if (selectedOperation === '-') {
+            isAccumulativeMode = true
+            if (b === '') b = a
+            calculateResult()
+        } else {
+            isAccumulativeMode = false
+        }
+        selectedOperation = '-'
+        b = ''
+    }
+
+    document.getElementById("orbit_div").onclick = function() {
+        if (a === '') return
+        selectedOperation = '/'
+    }
+
+    document.getElementById("aspect_sign").onclick = function() {
+        if (!selectedOperation && a !== '') {
+            a = ((+a) * -1).toString()
+            outputElement.innerHTML = a
+        } else if (selectedOperation && b !== '') {
+            b = ((+b) * -1).toString()
             outputElement.innerHTML = b
         }
     }
-}
 
-digitButtons.forEach(button => {
-    button.onclick = function() {
-        const digitValue = button.innerHTML
-        onDigitButtonClicked(digitValue)
-    }
-});
-
-document.getElementById("btn_op_mult").onclick = function() {
-    if (a === '') return
-    selectedOperation = 'x'
-}
-document.getElementById("btn_op_plus").onclick = function() {
-    if (a === '') return
-    if (selectedOperation === '+') {
-        isAccumulativeMode = true
-
-        if (b === '') {
-            b = a
-        }
-        calculateResult()
-    } else {
-        isAccumulativeMode = false
-    }
-    selectedOperation = '+'
-    lastOperand = a
-    b = ''
-}
-document.getElementById("btn_op_minus").onclick = function() {
-    if (a === '') return
-    if (selectedOperation === '-') {
-        isAccumulativeMode = true
-
-        if (b === '') {
-            b = a
-        }
-        calculateResult()
-    } else {
-        isAccumulativeMode = false
-    }
-    selectedOperation = '-'
-    lastOperand = a
-    b = ''
-}
-document.getElementById("btn_op_div").onclick = function() {
-    if (a === '') return
-    selectedOperation = '/'
-}
-
-document.getElementById("btn_op_sign").onclick = function() {
-    if (!selectedOperation && a !== '') {
-        a = ((+a) * -1).toString()
-        outputElement.innerHTML = a
-
-    } else if (selectedOperation && b !== '') {
-        b = ((+b) * -1).toString()
-        outputElement.innerHTML = b
-
-    } else if (lastResult !== '') {
-        lastResult = ((+lastResult) * -1).toString()
-        a = lastResult
-        outputElement.innerHTML = a
-    }
-}
-
-document.getElementById("btn_op_inverse").onclick = function() {
-    if (!selectedOperation && a !== '') {
-        if ((+a) === 0) {
+    document.getElementById("orbit_inverse").onclick = function() {
+        let target = (selectedOperation && b !== '') ? b : a
+        if (target === '' || (+target) === 0) {
             outputElement.innerHTML = 'Ошибка'
             return
         }
+        let res = (1 / (+target)).toString()
+        if (!selectedOperation) a = res; else b = res;
+        outputElement.innerHTML = res
+    }
 
-        a = (1 / (+a)).toString()
-        outputElement.innerHTML = a
+    document.getElementById("cosmic_del").onclick = function() {
+        if (!selectedOperation && a !== '') {
+            a = a.slice(0, -1)
+            outputElement.innerHTML = a || '0'
+        } else if (selectedOperation && b !== '') {
+            b = b.slice(0, -1)
+            outputElement.innerHTML = b || '0'
+        }
+    }
 
-    } else if (selectedOperation && b !== '') {
-        if ((+b) === 0) {
+    document.getElementById("orbit_sqrt").onclick = function() {
+        let target = (selectedOperation && b !== '') ? b : a
+        if (target === '' || (+target) < 0) {
             outputElement.innerHTML = 'Ошибка'
             return
         }
-
-        b = (1 / (+b)).toString()
-        outputElement.innerHTML = b
-
-    } else if (lastResult !== '') {
-
-        if ((+lastResult) === 0) {
-            outputElement.innerHTML = 'Ошибка'
-            return
-        }
-
-        lastResult = (1 / (+lastResult)).toString()
-        a = lastResult
-        selectedOperation = null
-        outputElement.innerHTML = a
-    }
-}
-
-document.getElementById("btn_op_del").onclick = function() {
-    if (!selectedOperation && a !== '') {
-        a = a.slice(0, -1)
-        outputElement.innerHTML = a
-
-    } else if (selectedOperation && b !== '') {
-        b = b.slice(0, -1)
-        outputElement.innerHTML = b
-    }
-}
-
-document.getElementById("btn_op_sqrt").onclick = function() {
-    if (!selectedOperation && a !== '') {
-        a = Math.sqrt((+a)).toString()
-        outputElement.innerHTML = a
-
-    } else if (selectedOperation && b !== '') {
-        b = Math.sqrt((+b)).toString()
-        outputElement.innerHTML = b
-
-    } else if (lastResult !== '') {
-        lastResult = Math.sqrt((+lastResult)).toString()
-        a = lastResult
-        selectedOperation = null
-        outputElement.innerHTML = a
-    }
-}
-
-document.getElementById("btn_op_secdg").onclick = function() {
-    if (!selectedOperation && a !== '') {
-        a = Math.pow((+a), 2).toString()
-        outputElement.innerHTML = a
-
-    } else if (selectedOperation && b !== '') {
-        b = Math.pow((+b), 2).toString()
-        outputElement.innerHTML = b
-
-    } else if (lastResult !== '') {
-        lastResult = Math.pow((+lastResult), 2).toString()
-        a = lastResult
-        selectedOperation = null
-        outputElement.innerHTML = a
-    }
-}
-
-document.getElementById("btn_op_clear").onclick = function() {
-    a = ''
-    b = ''
-    selectedOperation = ''
-    expressionResult = ''
-    outputElement.innerHTML = 0
-}
-
-function calculateResult() {
-    if (a === '' || b === '' || !selectedOperation) {
-        return
-    }
-    switch(selectedOperation) {
-        case '+':
-            expressionResult = (+a) + (+b)
-            break
-        case '-':
-            expressionResult = (+a) - (+b)
-            break
-        case 'x':
-            expressionResult = (+a) * (+b)
-            break
-        case '/':
-            expressionResult = (+a) / (+b)
-            break
+        let res = Math.sqrt((+target)).toString()
+        if (!selectedOperation) a = res; else b = res;
+        outputElement.innerHTML = res
     }
 
-    a = expressionResult.toString()
-    if (isAccumulativeMode) {
-        b = a
-    } else {
+    document.getElementById("orbit_secdg").onclick = function() {
+        let target = (selectedOperation && b !== '') ? b : a
+        if (target === '') return
+        let res = Math.pow((+target), 2).toString()
+        if (!selectedOperation) a = res; else b = res;
+        outputElement.innerHTML = res
+    }
+
+    document.getElementById("cosmic_clear").onclick = function() {
+        a = ''
         b = ''
-    }
-    outputElement.innerHTML = a
-}
-
-document.getElementById("btn_op_equal").onclick = function() {
-    calculateResult()
-    isAccumulativeMode = false
-}
-
-document.getElementById("btn_op_factorial").onclick = function() {
-    let num
-    if (!selectedOperation && a !== '') {
-        num = (+a)
-
-    } else if (selectedOperation && b !== '') {
-        num = (+b)
-
-    } else if (lastResult !== '') {
-        num = (+lastResult)
-
-    } else {
-        return
+        selectedOperation = ''
+        expressionResult = ''
+        outputElement.innerHTML = 0
     }
 
-    if (num < 0) {
-        outputElement.innerHTML = 'Ошибка'
-        return
-    }
+    function calculateResult() {
+        if (a === '' || b === '' || !selectedOperation) return
 
-    let factorial = fact(num)
+        let numA = parseFloat(a)
+        let numB = parseFloat(b)
 
-    if (!selectedOperation && a !== '') {
-        a = factorial.toString()
+        switch(selectedOperation) {
+            case '+': expressionResult = numA + numB; break
+            case '-': expressionResult = numA - numB; break
+            case 'x': expressionResult = numA * numB; break
+            case '/':
+                expressionResult = numB !== 0 ? numA / numB : 'Ошибка';
+                break
+        }
+
+        a = expressionResult.toString()
+        b = isAccumulativeMode ? a : ''
         outputElement.innerHTML = a
+    }
 
-    } else if (selectedOperation && b !== '') {
-        b = factorial.toString()
-        outputElement.innerHTML = b
-
-    } else if (lastResult !== '') {
-        lastResult = factorial.toString()
-        a = lastResult
+    document.getElementById("oracle_equal").onclick = function() {
+        calculateResult()
+        isAccumulativeMode = false
         selectedOperation = null
-        outputElement.innerHTML = a
     }
-}
 
-const themeToggle = document.getElementById("btn_switch_theme")
-
-document.body.className = 'dark-theme'
-themeToggle.innerHTML = 'Светлая тема'
-
-themeToggle.onclick = function() {
-    if (document.body.classList.contains('dark-theme')) {
-        document.body.className = 'light-theme'
-        themeToggle.innerHTML = 'Темная тема'
-    } else {
-        document.body.className = 'dark-theme'
-        themeToggle.innerHTML = 'Светлая тема'
+    document.getElementById("orbit_factorial").onclick = function() {
+        let target = (selectedOperation && b !== '') ? b : a
+        if (target === '') return
+        let res = fact(+target).toString()
+        if (!selectedOperation) a = res; else b = res;
+        outputElement.innerHTML = res
     }
-}
 
+    const themeToggle = document.getElementById("btn_nebula_theme")
+    document.body.className = 'dark-theme'
+    themeToggle.innerHTML = 'Светлая тема'
+
+    themeToggle.onclick = function() {
+        if (document.body.classList.contains('dark-theme')) {
+            document.body.className = 'light-theme'
+            themeToggle.innerHTML = 'Темная тема'
+        } else {
+            document.body.className = 'dark-theme'
+            themeToggle.innerHTML = 'Светлая тема'
+        }
+    }
 };
